3506269e2d0f8bc31017d9eade7a3a9e08b33309
Cole <2bigshot@iMac.local>
2018-07-05 01:31:38 -0600

    update

--- lesson-11/Lesson4/js/scripts.js
+++ lesson-11/Lesson4/js/scripts.js
@@ -20,4 +20,16 @@ $(function () {
 
     $('#ReqAQuoteBtn').click(toggleStyle);
 
+    $('.coreValues').prepend("<li><strong>This is brand new !!</strong></li>")
+     $("<li><strong>This is also great !!</strong></li>").prependTo('.coreValues')
+    
+    var nameVal = $('input[name="nameVal"]');
+    
+    function fillEmpty(){
+        if(nameVal.val() == ''){
+            nameVal.val('John Doe');
+        }
+    }
+    
+    $('#ReqAQuoteBtn').click(fillEmpty);
 })
