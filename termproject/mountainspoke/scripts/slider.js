8e1370c3fa5826372ed72182e7834de1e4ae526e
Cole <2bigshot@iMac.local>
2018-07-22 23:25:03 -0600

    almost done

--- termproject/mountainspoke/scripts/slider.js
+++ termproject/mountainspoke/scripts/slider.js
@@ -1,30 +1,40 @@
 $(document).ready(function() {
 
     var which = 1;
+    var transitionSpeed = 100;
 
     $("#left").click(function() {
 
-        $("#bike" + which).fadeOut(350, function() {
+        $("#left").attr("id", "leftfrozen");
+        $("#right").attr("id", "rightfrozen");
+
+        $("#bike" + which).fadeOut(transitionSpeed, function() {
 
             which--;
             if(which < 1)
             {
                 which = 3;
             }
-                $("#bike" + which).fadeIn(350);
+            $("#bike" + which).fadeIn(transitionSpeed, function(){
+
+            });
         });
     });
 
     $("#right").click(function() {
 
-        $("#bike" + which).fadeOut(350, function() {
+        $("#right").attr("id", "rightfrozen");
+        $("#left").attr("id", "leftfrozen");
+
+        $("#bike" + which).fadeOut(transitionSpeed, function() {
 
             which++;
             if(which > 3)
             {
                 which = 1;
             }
-                $("#bike" + which).fadeIn(350);
+            $("#bike" + which).fadeIn(transitionSpeed, function(){
+            });
         });
     });
 });
