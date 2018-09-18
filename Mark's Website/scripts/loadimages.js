2187c961386607344f7706c9eee32e2181bce30f
Cole <2bigshot@iMac.local>
2018-09-18 01:45:54 -0600

    Update

--- Mark's Website/scripts/loadimages.js
+++ Mark's Website/scripts/loadimages.js
@@ -2,34 +2,47 @@ function loadImages(begin){
 
     var folder = "images/watercolorpaintings";
     var load = 20;
-    var counter = 0;
+    var loadAmount = 0;
+    var total = 0;
     var start = begin * 20;
-    var numbuttons = 0;
+    var numButtons = 0;
 
     $.ajax({
         url : folder,
         success: function (data) {
 
+            var myNode = document.getElementById("gridcontainer");
+            while (myNode.firstChild) {
+                myNode.removeChild(myNode.firstChild);
+            }
+            var myNode = document.getElementById("buttoncontainer");
+            while (myNode.firstChild) {
+                myNode.removeChild(myNode.firstChild);
+            }
+
             $(data).find("a").attr("href", function (i, val) {
                 if( val.match(/\.(jpe?g|JPG|png|gif)$/) ) {
-                    //loads 20 images at a time
-                    counter++;
-                    if(counter < load && counter <= start){
-                        $(".grid-container").append( "<div class='grid-img'> <img src='" + val +"'></div>" + counter);
-                    } 
+                    start--
+                    total++;
+                    if(start <= -1){
+                        //loads 20 images at a time
+                        loadAmount++;
+                        if(loadAmount <= load){
+                            $(".grid-container").append( "<div class='grid-img'> <img src='" + val +"'></div>");
+                        }} 
                 }});
-            
+
             //creates buttons
-            if(counter % 20 == 0)
+            if(total % 20 == 0)
             {
-                numbuttons = counter / 20;
+                numButtons = total / 20;
             }
             else
             {
-                numbuttons = counter / 20 + 1;
+                numButtons = total / 20 + 1;
             }
 
-            for(var i = 1; i < numbuttons; i++)
+            for(var i = 1; i < numButtons; i++)
             {
                 $(".imagebutton-holder").append("<button onclick='loadImages(" + (i - 1) + ")'>" + i + "</button>");
             }
