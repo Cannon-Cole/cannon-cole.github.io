7883b24e9df01eb8d0fe03d1ef682198f4256b2e
Cannon-Cole <colecannonschool@gmail.com>
2022-02-18 23:25:32 -0700

    Remove submodule and photo test site

--- mark/scripts/lightbox.js
+++
@@ -1,9 +0,0 @@
-function createLightbox(id)
-{
-    $(".grid-container").append("<div class='lightbox'><img src='/images/large/"+ id +".jpg'><button onclick='destroyLightbox()'></button></div>");
-}
-
-function destroyLightbox()
-{
-    $(".lightbox").remove();
-}
